923f00f27218070b6befb74ae4e7aed93597bc6f
saify <sarfaraz.symits@gmail.com>
2025-02-04 21:07:49 +0500

    working on layout

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { FaMapMarkerAlt } from "react-icons/fa";
 import { FaQuoteLeft, FaQuoteRight, FaRegCirclePlay } from "react-icons/fa6";
 import { LiaCheckDoubleSolid } from "react-icons/lia";
+import { AiOutlineMail } from "react-icons/ai";
 import { GiInjustice } from "react-icons/gi";
+import { MdOutlinePhone } from "react-icons/md";
 import {
   BsBoundingBoxCircles,
   BsCalendar2Week,
   BsActivity,
+  BsMap,
 } from "react-icons/bs";
 import BaseLayout from "@/layout/base";
 import Counter from "@/components/counter";
@@ -532,6 +536,146 @@ export default function () {
         </div>
       </section>
       {/* Call to action */}
+
+      {/* Contact */}
+      <section id="contact" class="contact section">
+        <div class="container section-title" data-aos="fade-up">
+          <span>Section Title</span>
+          <h2>Contact</h2>
+          <p>
+            Necessitatibus eius consequatur ex aliquid fuga eum quidem sint
+            consectetur velit
+          </p>
+        </div>
+
+        <div class="container" data-aos="fade-up" data-aos-delay="100">
+          <div class="row gy-4">
+            <div class="col-lg-5">
+              <div class="info-wrap">
+                <div
+                  class="info-item d-flex"
+                  data-aos="fade-up"
+                  data-aos-delay="200"
+                >
+                  <FaMapMarkerAlt size="20" className="flex-shrink-0"/>
+                  <div>
+                    <h3>Address</h3>
+                    <p>A108 Adam Street, New York, NY 535022</p>
+                  </div>
+                </div>
+
+                <div
+                  class="info-item d-flex"
+                  data-aos="fade-up"
+                  data-aos-delay="300"
+                >
+                 <MdOutlinePhone size="20" className="flex-shrink-0"/>
+                  <div>
+                    <h3>Call Us</h3>
+                    <p>+1 5589 55488 55</p>
+                  </div>
+                </div>
+
+                <div
+                  class="info-item d-flex"
+                  data-aos="fade-up"
+                  data-aos-delay="400"
+                >
+                 <AiOutlineMail size="20" className="flex-shrink-0"/>
+                  <div>
+                    <h3>Email Us</h3>
+                    <p>info@example.com</p>
+                  </div>
+                </div>
+
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus"
+                  frameborder="0"
+                  style={{ border: 0, width: "100%", height: 270 }}
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
+            </div>
+
+            <div class="col-lg-7">
+              <form
+                action="forms/contact.php"
+                method="post"
+                class="php-email-form"
+                data-aos="fade-up"
+                data-aos-delay="200"
+              >
+                <div class="row gy-4">
+                  <div class="col-md-6">
+                    <label for="name-field" class="pb-2">
+                      Your Name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name-field"
+                      class="form-control"
+                      required=""
+                    />
+                  </div>
+
+                  <div class="col-md-6">
+                    <label for="email-field" class="pb-2">
+                      Your Email
+                    </label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      name="email"
+                      id="email-field"
+                      required=""
+                    />
+                  </div>
+
+                  <div class="col-md-12">
+                    <label for="subject-field" class="pb-2">
+                      Subject
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      name="subject"
+                      id="subject-field"
+                      required=""
+                    />
+                  </div>
+
+                  <div class="col-md-12">
+                    <label for="message-field" class="pb-2">
+                      Message
+                    </label>
+                    <textarea
+                      class="form-control"
+                      name="message"
+                      rows="10"
+                      id="message-field"
+                      required=""
+                    ></textarea>
+                  </div>
+
+                  <div class="col-md-12 text-center">
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">
+                      Your message has been sent. Thank you!
+                    </div>
+
+                    <button type="submit">Send Message</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Contact */}
     </BaseLayout>
   );
 }
