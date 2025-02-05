72a2341e248e809788be6006014d605b540a26e4
saify <dev.muhammadsarfaraz@gmail.com>
2025-02-05 22:25:39 +0500

    laywers page

--- components/lawyer.jsx
+++ components/lawyer.jsx
@@ -1,34 +1,33 @@
 import { BsStar, BsStarFill } from "react-icons/bs";
 import styles from "./lawyer.module.css";
 
-export default function LawyerCard(props) {
-  const { avatar, name, qualification, rating, className } = props;
-
+export default function LawyerCard({ avatar, name, qualification, rating, className }) {
   return (
     <div className={`${styles.lawyer} ${className}`}>
       <div className={styles.avatar}>
-        <img className="img-fluid" src={avatar} alt="lawyer" />
+        <img className="img-fluid" src={avatar} alt={`${name}'s profile`} />
       </div>
-      <div>
+      <div className={styles.content}>
         <h3 className={styles.title}>{name}</h3>
         <small className={styles.qualification}>{qualification}</small>
         <div className="d-flex gap-1 mt-2">
-          {[...Array(5)].map((_, index) =>
-            index < rating ? (
-              <BsStarFill key={index} style={{color:'#FFD700'}}/>
-            ) : (
-              <BsStar key={index} style={{color:'#FFD700'}}/>
-            )
-          )}
+          {Array(5)
+            .fill()
+            .map((_, index) =>
+              index < rating ? (
+                <BsStarFill key={index} className={styles.star} />
+              ) : (
+                <BsStar key={index} className={styles.star} />
+              )
+            )}
         </div>
       </div>
-
       <div className={styles.action}>
-        <a href="#" className="btn btn-outline-info rounded-0">
-          View profile
+        <a href="#" className="btn btn-outline-info">
+          View Profile
         </a>
-        <a href="#" className="btn btn-warning rounded-0 text-light">
-          Book appointment
+        <a href="#" className="btn btn-warning text-light">
+          Book Appointment
         </a>
       </div>
     </div>
