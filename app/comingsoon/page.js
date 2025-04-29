17e42c41b931047f9d0c888bd820f706bd82db55
saify <dev.muhammadsarfaraz@gmail.com>
2025-04-29 23:01:48 +0500

    typo fixed

--- app/comingsoon/page.js
+++ app/comingsoon/page.js
@@ -1,7 +1,7 @@
 export default function(){
     return<>
     <div style={{display: 'grid', placeContent: 'center', height: '100vh'}}>
-        <h2>Comming soom!</h2>
+        <h2>Comming soon!</h2>
     </div>
     </>
 }
